7db7fb9f8fb29d0c794fc85e4e750f2dfdc8747c
skarthik05 <karthiks0598@gmail.com>
2022-07-03 18:47:53 +0530

    added few basic methods of test

--- function.js
+++ function.js
@@ -1,6 +1,11 @@
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
+  checkValue: (value) => value,
+  createUser: () => {
+    firstName: "J";
+    lastName: "S";
+  },
 };
 
 module.exports = functions;
--- function.test.js
+++ function.test.js
@@ -1,11 +1,38 @@
 const functions = require("./function");
-
+//toBe
 test("Add two number", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 test("Add 1 + 1 to NOT equal 3", () => {
   expect(functions.add(2, 2)).not.toBe(3);
 });
-test("To be NULL", () => {
+//toBeNull
+test("Should be NULL", () => {
   expect(functions.isNull()).toBeNull();
+});
+//toBeFalsy
+test("Should be falsy", () => {
+  expect(functions.checkValue(0)).toBeFalsy();
+});
+//toEqual
+test("User Should be JS Object", () => {
+  expect(functions.createUser()).toEqual();
+});
+//Less than and greater then
+
+test("Should be under 1000", () => {
+  const load1 = 100;
+  const load2 = 900;
+  expect(load1 + load2).toBeLessThanOrEqual(1000);
+});
+
+//Regex
+test("There is no $ in i write code", () => {
+  expect("i write code").not.toMatch(/[$]/);
+});
+
+//Arrays
+test("Admin Should be in username", () => {
+  const username = ["user", "guest", "admin"];
+  expect(username).toContain("admin");
 });
